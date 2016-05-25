175f67a851c44062f6aba7e9d122495fb55f77dc
Bibek <jujulalshrestha@gmail.com>
2016-05-25 17:52:34 -0500

    SuffleSort started

--- BigData/src/assignment/Lab3/WordCount.java
+++ BigData/src/assignment/Lab3/WordCount.java
@@ -1,12 +1,27 @@
 package assignment.Lab3;
 
+import assignment.Lab2.MyMapper;
+import assignment.Lab2.MyReducer;
+
 public class WordCount {
-	private int noOfMapper; 
-	private int noOfReducer; 
-	private MyMapper mapper[];
-	private MyReducer reducer[];
+
+	private MyMapper mappers[];
+	private MyReducer reducers[];
 	
-	WordCount(int m, int r){
+	public WordCount(int m, int r){
+		mappers = new MyMapper[m];
+		reducers = new MyReducer[r];
+	}
+	
+	public void shuffleSort(){
+		//initialing mappers instance
+		for(int i=0;i<mappers.length;i++){
+			mappers[i]= new MyMapper();
+		}
+		//initialing reducers instance
+		for(int i=0;i<reducers.length;i++){
+			reducers[i]=new MyReducer();
+		}
 		
 	}
 }
