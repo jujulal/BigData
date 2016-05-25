ab03dd4f7745ecf875491243dda103bf5579209d
jujulal <jujulalshrestha@gmail.com>
2016-05-25 14:49:03 -0500

    Lab3 start

---
+++ BigData/src/assignment/Lab3/WordCount.java
@@ -0,0 +1,12 @@
+package assignment.Lab3;
+
+public class WordCount {
+	private int noOfMapper; 
+	private int noOfReducer; 
+	private MyMapper mapper[];
+	private MyReducer reducer[];
+	
+	WordCount(int m, int r){
+		
+	}
+}
