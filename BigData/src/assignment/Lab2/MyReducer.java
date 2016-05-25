bee1975b6d95b66330e2f9fc157313e16582ef4c
Bibek <jujulalshrestha@gmail.com>
2016-05-24 23:10:08 -0500

    Signed-off-by: Bibek <jujulalshrestha@gmail.com>

--- BigData/src/assignment/Lab2/MyReducer.java
+++ BigData/src/assignment/Lab2/MyReducer.java
@@ -1,42 +1,7 @@
 package assignment.Lab2;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MyReducer {
-	Map mapString = new HashMap();
-
-	public void maperInput() {
-		FileReader fileReader;
-		String line;
-		try {
-			fileReader = new FileReader("c:/txtFile/testDataForW1D1.txt");
-			BufferedReader bufferReader = new BufferedReader(fileReader);
-
-			while ((line = bufferReader.readLine()) != null) {
-
-				String[] tokens = line.split(" ");
-
-				for (int i = 0; i < tokens.length; i++) {
-					if (isWord(tokens[i]) != null) {
-						stringMap.put(tokens[i], 1);
-						// System.out.println(tokens[i]);
-					}
-
-				}
-			}
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
+	
 
 }
--- BigData/src/assignment/Lab2/Pair.java
+++ BigData/src/assignment/Lab2/Pair.java
@@ -1,54 +1,35 @@
 package assignment.Lab2;
 
-public class Pair<K,V> {
-	private K key; 
-	private V value;
+public class Pair<k extends Comparable<k>,v> implements Comparable<Pair<k,v>> {
+	private k key; 
+	private v value;
 	
-	public Pair(K key, V value){
+	public Pair(k key, v value){
 		this.key =key;
 		this.value = value;
 	}
-	public K getKey() {
+	public k getKey() {
 		return key;
 	}
-	public void setKey(K key) {
+	public void setKey(k key) {
 		this.key = key;
 	}
-	public V getValue() {
+	public v getValue() {
 		return value;
 	}
-	public void setValue(V value) {
+	public void setValue(v value) {
 		this.value = value;
 	}
 	
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((key == null) ? 0 : key.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
+	public String toString(){
+		return "(" + key + "," + value + ")";
 	}
+	
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pair other = (Pair) obj;
-		if (key == null) {
-			if (other.key != null)
-				return false;
-		} else if (!key.equals(other.key))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	} 
+	public int compareTo(Pair<k, v> o) {
+		// TODO Auto-generated method stub
+		return key.compareTo(o.getKey());
+	}
 	
 }
